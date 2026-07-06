0aa00e3e0c36b1886eb79761decd7a6089354dcb
elitemediagroup1 <matt@elitemediagroup.io>
2026-07-05 20:33:26 -0400

    PR #57: Add Loop OS design system barrel (index)
    Refactor only. Barrel re-export of the Loop OS design system. No visual/behavior/data changes.

---
+++ apps/web/src/app/app/_loop-os/index.ts
@@ -0,0 +1,6 @@
+export type { Tone, Ranked } from "./types";
+export { money, num, greeting, todayLabel, relTime, clockDuration, sparkPath } from "./format";
+export { StatusDot, Sparkline } from "./primitives";
+export { Module, Bar, RankedList } from "./modules";
+export { AttentionRow, BriefingItem, IntegrationPill } from "./panels";
+export { ActionTile } from "./launchers";
